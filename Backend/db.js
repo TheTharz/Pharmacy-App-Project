b5e8ade9bad210caaff1553274e8b8df68026ce3
Theshikanavod <theshikanavod@gmail.com>
2024-02-17 07:45:48 +0530

    commit 1

---
+++ Backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+
+    try {
+        mongoose.connect(process.env.DB, connectionParams);
+        console.log("Connected to database successfully");
+    } catch (error) {
+        console.error("Error:", error.message);
+        console.log("Couldn't connect to database");
+    }
+};
