a0eb9fb48b4c8c05e2d2840b2add586c4b5aa098
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-03-24 02:20:11 +0530

    order placing logic is working

--- frontend/src/components/customer-dashboard/components/Categories.jsx
+++ frontend/src/components/customer-dashboard/components/Categories.jsx
@@ -22,7 +22,7 @@ const Categories = () => {
 
   return (
     <div className='container mx-auto mt-8'>
-      <h1 className='text-2xl font-bold mb-4'>Categories</h1>
+      <h1 className='text-2xl font-bold mb-4'>All Medicine</h1>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
         {medicines.map((medicine, index) => (
           <div key={index} className='bg-white rounded-lg shadow-md p-6'>
