62f1af763e28601c90390a4c4c1c7d9dcbbf3e28
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-03-23 22:54:31 +0530

    Add admin routes and remove order routes

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const connection = require('./db');
 const userRoutes = require('./routes/user');
 const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin');
 
 //database connection
 connection();
@@ -16,6 +17,7 @@ app.use(cors());
 //routs
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`listening on port ${port}...`));
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -2,10 +2,13 @@
 const router = require('express').Router();
 
 const { getAllMedicine } = require('../controllers/MedicineController');
+const { addOrder } = require('../controllers/OrderController');
 const { signUp } = require('../controllers/UserController');
 
 router.route('/').post(signUp);
 
 router.route('/get-all-medicine').get(getAllMedicine);
+
+router.route('/place-order').post(addOrder);
 
 module.exports = router;
